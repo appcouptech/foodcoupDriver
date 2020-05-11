cf1783fe5714946eb51b17b18140467f44a4d3f4
kamal-appcoup <kamal@appcoup.com>
2020-05-11 17:29:00 +0530

    hello1

--- app/src/main/java/foodcoup/driver/demand/MainActivity.java
+++ app/src/main/java/foodcoup/driver/demand/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("dfgdfgd","fdgdf");
+        Log.d("dfgdfgd","fdgdf");
     }
 }
